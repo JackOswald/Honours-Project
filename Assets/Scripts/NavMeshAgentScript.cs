1fe845ad7ea401a9fe2b2d7434b870784f67daca
JackOswald <jack_oswald_cfc@hotmail.com>
2018-02-07 20:49:48 +0000

    Added Enemy Movement

---
+++ Assets/Scripts/NavMeshAgentScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentScript : MonoBehaviour {
+
+	public Transform[] targets;
+	public int destPoint = 0;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent <NavMeshAgent> ();
+		agent.autoBraking = false;
+		MoveToNextPoint ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//agent.SetDestination (target.position);	
+		if (!agent.pathPending && agent.remainingDistance < 0.5f)
+		{
+			MoveToNextPoint ();
+		}
+	}
+
+	void MoveToNextPoint()
+	{
+		if (targets.Length == 0) 
+		{
+			return;
+		}
+
+		agent.destination = targets [destPoint].position;
+
+		//destPoint = (destPoint + 1) % targets.Length;
+		destPoint = Random.Range(0, targets.Length);
+	}
+}
